7c79c516533bac52f8de607a157dbc98aecfdda3
alvaromd99 <117214600+alvaromd99@users.noreply.github.com>
2023-10-05 16:51:56 +0200

    clear project

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
 import './App.css'
+import WeatherInfo from './components/WeatherInfo'
+import { WeatherData } from './types/types'
 
 function App() {
-  const [count, setCount] = useState(0)
+	const [data, setData] = useState<WeatherData[]>([])
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+	useEffect(() => {
+		const fetchData = async () => {
+			try {
+				const response = await fetch(
+					'https://api.openweathermap.org/data/2.5/weather?q=Helsinki&appid=0e6517a3cada027943a4ce280e2eacd3'
+				)
+				if (!response.ok) {
+					throw new Error(`HTTP error! Status: ${response.status}`)
+				}
+				const responseData = await response.json()
+				setData(responseData)
+			} catch (err) {
+				console.error(err)
+			}
+		}
+
+		fetchData()
+	}, [])
+
+	console.log(data)
+
+	return (
+		<div className='App'>
+			<WeatherInfo />
+		</div>
+	)
 }
 
 export default App
---
+++ src/components/WeatherInfo.tsx
@@ -0,0 +1,7 @@
+export default function WeatherInfo() {
+	return (
+		<div className='city-info'>
+			<h1>hello</h1>
+		</div>
+	)
+}
