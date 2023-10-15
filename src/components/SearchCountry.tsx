769fe0f5c9af0a79b4c18bfa1265076201c5037c
alvaromd99 <117214600+alvaromd99@users.noreply.github.com>
2023-10-15 22:03:44 +0200

    start css on search comp

--- src/components/SearchCountry.tsx
+++ src/components/SearchCountry.tsx
@@ -1,3 +1,12 @@
+import '../styles/SearchCountry.css'
+
 export default function SearchCountry() {
-	return <div className='search-container'>SearchCountry</div>
+	return (
+		<div className='search-container'>
+			<form onSubmit={() => console.log('hello')}>
+				<input type='text' />
+				<input type='submit' />
+			</form>
+		</div>
+	)
 }
